03f5f435ddd38a50104f14a6516f08042663396c
Rachel Kelley <rachakelley@gmail.com>
2017-11-29 17:39:07 -0500

    Update RomanCalculatorGUI.java

--- RomanCalculatorGUI.java
+++ RomanCalculatorGUI.java
@@ -1,11 +1,6 @@
 /*
- * Rachel Kelley
- * This class contains all the code/logic for the roman calculator GUI
+ * Class containing all the code/logic for the roman calculator GUI
  * and it's functions.
- * The roman numeral calculator allows you to add, subtract, divide,
- * mod, and multiply roman numerals. The integer representation of each
- * operand as well as the result is shown in addition to the roman numeral
- * representations.
 */
 
 import java.awt.*;
